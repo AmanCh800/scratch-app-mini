392d4d3bd06360e46106c272b1585295dcb0ed24
Aman Chadha <76766472+AmanCh800@users.noreply.github.com>
2024-06-27 12:23:19 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  darkMode: "class",
+  purge: ["./src/**/*.js"],
+  variants: {},
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
